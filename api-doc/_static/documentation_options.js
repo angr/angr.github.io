8670f3a049cf8114445a7f65d42b635df5cfc8e4
Audrey Dutcher <audrey@rhelmot.io>
2018-02-24 23:56:09 -0800

    Mid-cycle docs bugfix update

---
+++ api-doc/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '7.8.2.21',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
